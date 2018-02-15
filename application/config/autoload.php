fb38395b527048930b81985cc0467854e45bfa7d
clai <clai-@hotmail.com>
2018-02-15 15:07:13 -0800

    job3 v1

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -29,7 +29,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | 7. Models
 |
 */
-$autoload['model'] = array('app', 'tasks');
 
 /*
 | -------------------------------------------------------------------
@@ -134,3 +133,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array('app');
+$autoload['model'] = array('app', 'tasks');
