ed64760b807f242be203bd8bd7d44a052402f17a
clai <clai-@hotmail.com>
2018-03-01 14:41:13 -0800

    job8 completed, check changelog.md for details
    Signed-off-by: clai <clai-@hotmail.com>

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -28,12 +28,16 @@ class Views extends Application {
             $converted[] = (array) $task;
 
         // and then pass them on
+        $role = $this->session->userdata('userrole');
         $parms = ['display_tasks' => $converted];
+        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
         return $this->parser->parse('by_priority', $parms, true);
     }
 
     function makeCategorizedPanel($tasks) {
         $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
+        $role = $this->session->userdata('userrole');
+        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
         return $this->parser->parse('by_category', $parms, true);
     }
 
@@ -47,4 +51,21 @@ class Views extends Application {
             return 0;
     }
 
+    // complete flagged items
+    function complete() {
+        $role = $this->session->userdata('userrole');
+        if ($role != ROLE_OWNER) 
+            redirect('/views');
+
+        foreach($this->input->post() as $key=>$value) {
+            if (substr($key,0,4) == 'task') {
+                // find the associated task
+                $taskid = substr($key,4);
+                $task = $this->tasks->get($taskid);
+                $task->status = 2; // complete
+                $this->tasks->update($task);
+            }
+        }   
+        $this->index();
+    }
 }
--- application/views/by_category.php
+++ application/views/by_category.php
@@ -1,16 +1,20 @@
 <h3>Tasks by Category</h3>
-
+<form method='POST' action='{completer}'>
 <table class="table">
-        <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Category</th>
-        </tr>
-        {display_tasks}
-        <tr>
-            <td>{id}</td>
-            <td>{task}</td>
-        <td>{group}</td>
-        </tr>
-        {/display_tasks}
+    <tr>
+        <th>Id</th>
+        <th></th>   <!-- INSERT this line -->
+        <th>Task</th>
+        <th>Priority</th>
+    </tr>
+    {display_tasks}
+    <tr>
+        <td>{id}</td>
+        <td><input type='checkbox' name='task{id}'/></td>
+        <td>{task}</td>
+        <td>{priority}</td>
+    </tr>
+    {/display_tasks}
 </table>
+<input type='submit' value='Complete checked tasks'/>
+</form>
