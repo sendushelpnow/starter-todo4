cbdcb38c9c1b74a9de9596842c8ab0c72315bbd9
chapped <jyoon52@my.bcit.ca>
2018-03-03 23:41:20 -0800

    completed job 10
    Signed-off-by: chapped <jyoon52@my.bcit.ca>

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -2,42 +2,50 @@
 
 class Tasks extends CSV_Model {
 
-  public function __construct() {
-      parent::__construct(APPPATH . '../data/tasks.csv', 'id');
-  }
+    public function __construct() {
+        parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+    }
 
-  function getCategorizedTasks()
-  {
-      // extract the undone tasks
-      foreach ($this->all() as $task)
-      {
-          if ($task->status != 2)
-              $undone[] = $task;
-      }
+    // provide form validation rules
+    public function rules() {
+        $config = array(
+            ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+            ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
+            ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
+            ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
+        );
+        return $config;
+    }
 
-      // substitute the category name, for sorting
-      foreach ($undone as $task)
-          $task->group = $this->app->group($task->group);
+    function getCategorizedTasks() {
+        // extract the undone tasks
+        foreach ($this->all() as $task) {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
 
-      // order them by category
-      usort($undone, array("Tasks", "orderByCategory"));
+        // substitute the category name, for sorting
+        foreach ($undone as $task)
+            $task->group = $this->app->group($task->group);
 
-      // convert the array of task objects into an array of associative objects
-      foreach ($undone as $task)
-          $converted[] = (array) $task;
+        // order them by category
+        usort($undone, array("Tasks", "orderByCategory"));
 
-  return $converted;
-  }
+        // convert the array of task objects into an array of associative objects
+        foreach ($undone as $task)
+            $converted[] = (array) $task;
 
-  // return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
-  function orderByCategory($a, $b)
-  {
-      if ($a->group < $b->group)
-          return -1;
-      elseif ($a->group > $b->group)
-          return 1;
-      else
-          return 0;
-  }
+        return $converted;
+    }
+
+    // return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
+    function orderByCategory($a, $b) {
+        if ($a->group < $b->group)
+            return -1;
+        elseif ($a->group > $b->group)
+            return 1;
+        else
+            return 0;
+    }
 
 }
