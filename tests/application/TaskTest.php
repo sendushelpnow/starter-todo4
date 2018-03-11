8377f2976c008a0e7a65a3490cd33eee22c7b323
chapped <jyoon52@my.bcit.ca>
2018-03-11 15:53:07 -0700

    completed tasktest
    Signed-off-by: chapped <jyoon52@my.bcit.ca>

--- tests/application/TaskTest.php
+++ tests/application/TaskTest.php
@@ -8,11 +8,11 @@ class TaskTest extends TestCase {
         $this->CI = &get_instance();
         $this->CI->load->model('task');
         $this->item = new Task();
-        $this->task = "New task";
-        $this->priority = "low";
-        $this->size = "small";
-        $this->group = "house";
-        $this->status = "in progress";
+        $this->item->task = "New task";
+        $this->item->priority = "low";
+        $this->item->size = "small";
+        $this->item->group = "house";
+        $this->item->status = "in progress";
     }
 
     function testSetup() {
@@ -26,30 +26,58 @@ class TaskTest extends TestCase {
     }
 
     function testInvalidTask() {
-
+        //$this->expectException('InvalidArgumentException');
+        $this->item->task = null;
     }
     
-    function testTask() {
-        $expected = "new task";
+    function testTaskPresent() {
+        $expected = "Mow lawn";
         $this->item->task = $expected;
         $this->assertEquals($expected, $this->item->task);
     }
 
-    function testPriority() {
-            
+    function testTaskAbsent() {
+        $badvalue = "";
+        $this->expectException(Exception::class);
+        $this->item->task = $badvalue; // this should croak
+    }
+
+    function testTaskTooLong() {
+        $badvalue = "Mow the lawn. Make sure the grass is short but not too short. I think about"
+                . "5cm would be good. Actually, nevermind. Make it 1 cm. Actually... 1cm is too short."
+                . "Make it around 3cm. Yes, 3cm would be perfect.";
+        $this->expectException('A task cannot be longer than 30 characters');
+        $this->item->task = $badvalue; // this should croak
+    }
+
+    function testInvalidPriority() {
+        $badvalue = "kinda low";
+        $this->expectException('Priority must be either small, medium, or large');
+        $this->item->priority = $badvalue; // this should croak
     }
     
-    function testSize() {
+    function testInvalidSize() {
+        $badvalue = "too small";
+        $this->expectException('The size must be either small, medium, or large');
+        $this->item->size = $badvalue; // this should croak
 
     }
 
-    function testGroup() {
+    function testInvalidGroup() {
+        $badvalue = "party";
+        $this->expectException('Please choose one of the valid groups');
+        $this->item->group = $badvalue; // this should croak
 
     }
 
-    function testStatus() {
+    function testInvalidStatus() {
+        $badvalue = "almost done";
+        $this->expectException('A status must be either complete or in progress');
+        $this->item->status = $badvalue; // this should croak
 
     }
+    
+    
 
 
 }
